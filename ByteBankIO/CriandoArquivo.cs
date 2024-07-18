f2470409b798b2847d7a37fa426b38d9372c5753
Knust06 <lucasknust0605@gmail.com>
2024-07-18 18:26:39 -0300

    Entendendo o Flush e o porque a primeira vez da aplicação o programa não imprimia nada no arquivo teste.txt

---
+++ ByteBankIO/CriandoArquivo.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+
+partial class Program
+{
+    static void CriarArquivo()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+        using var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create);
+        var contaComoString = "456, 7895, 5487.09, Lucas Knust";
+
+        var encoding = Encoding.UTF8;
+
+        var bytes = encoding.GetBytes(contaComoString);
+
+        fluxoDeArquivo.Write(bytes, 0, bytes.Length);
+    }
+
+    static void CriarArquivoComWriter()
+    {
+        var caminhoNovoArquivo = "contasExportadas.csv";
+
+        using var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.CreateNew);
+        using var escritor = new StreamWriter(fluxoDeArquivo);
+        escritor.Write("456, 65465, 456.0, Lucas Knust");
+    }
+
+    static void TestaEscrita()
+    {
+        var caminhoNovoArquivo = "teste.txt";
+
+        using var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.Create);
+        using (var escritor = new StreamWriter(fluxoDeArquivo))
+        {
+            for (int i = 0; i < 1000000; i++)
+            {
+                escritor.WriteLine($"Linha {i}");
+                escritor.Flush(); //Despeja o buffer para o Stream!
+                Console.WriteLine($"Linha {i} foi escrita no arquivo. Tecle enter...");
+                Console.ReadLine();
+            }                        
+        }
+        
+    }
+
+}
---
+++ ByteBankIO/UsandoStreamReader.cs
@@ -0,0 +1,50 @@
+﻿using ByteBankIO;
+
+partial class Program
+{
+    static void UsandoStreamReader(string[] args)
+    {
+        var enderecoDoArquivo = "contas.txt";
+        using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
+        {
+            var leitor = new StreamReader(fluxoDeArquivo);
+
+            //var linha = leitor.ReadLine();
+            //var texto = leitor.ReadToEnd();
+            //var numero = leitor.Read();
+            while (!leitor.EndOfStream)
+            {
+                var linha = leitor.ReadLine();
+                var contaCorrente = ConverterStringContaCorrente(linha);
+                var msg = $"Conta número {contaCorrente.Numero}, ag. {contaCorrente.Agencia}, saldo {contaCorrente.Saldo}, titular {contaCorrente.Titular.Nome}";
+                Console.WriteLine(msg);
+            }
+        }
+        Console.ReadLine();
+    }
+    static ContaCorrente ConverterStringContaCorrente(string linha)
+    {
+        var campos = linha.Split(',');
+
+
+        var agencia = campos[0];
+        var numero = campos[1];
+        var saldo = campos[2];
+        var nomeTitular = campos[3];
+
+        var agenciaComInt = int.Parse(agencia);
+        var numeroComInt = int.Parse(numero);
+        var saldoComDouble = double.Parse(saldo);
+
+        var titular = new Cliente();
+        titular.Nome = nomeTitular;
+
+        var resultado = new ContaCorrente(agenciaComInt, numeroComInt);
+        resultado.Depositar(saldoComDouble);
+        resultado.Titular = titular;
+        return resultado;
+    }
+
+
+
+}
